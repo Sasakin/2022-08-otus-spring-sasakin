01072874ff57fd1ec6d2e46a0b9162e062626ae7
MSasakin <msasakin@it-one.ru>
2022-09-19 20:59:46 +0300

    make first HW

---
+++ 1-exam-project/src/main/java/ru/otus/spring/domain/Test.java
@@ -0,0 +1,14 @@
+package ru.otus.spring.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Test {
+
+    private String question;
+
+    private List<String> options;
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Репозиторий с выполненным ДЗ курса OTUS: "Разработчик Spring Framework"
+
+## ДЗ к уроку № 1 Введение в Spring Framework
+* 1-exam-project
